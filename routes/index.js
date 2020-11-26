31e55fc4866d6940843c8ea90a40c0c006a721d0
Aditya-Saini <aju180601@gmail.com>
2020-11-26 15:50:01 +0530

    add middle ware to calendar page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express=require("express");
 var router=express.Router();
 var passport=require("passport");
-var User=require("../models/user");
+var User=require("../models/user"),
+	middleware=require('../middleware');
 
-router.get("/", function(req, res){
+router.get("/", middleware.isLoggedIn, function(req, res){
 	res.render("calendar");
 });
 
